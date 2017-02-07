253f3ba4d3543c12961eae2fea44598b76027808
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-06 22:04:12 -0300

    changed buld options for go app

--- ci/scripts/deploy-service-broker.sh
+++ ci/scripts/deploy-service-broker.sh
@@ -6,5 +6,5 @@ set -e -x
 cp -r service-broker-repo/ go/src/github.com/Altoros/cf-chaos-loris-broker
 export GOPATH=$PWD/go/ 
 cd go/src/github.com/Altoros/cf-chaos-loris-broker
-go get -d -t -v github.com/Altoros/cf-chaos-loris-broker
+#go get -d -t -v github.com/Altoros/cf-chaos-loris-broker
 go build 
