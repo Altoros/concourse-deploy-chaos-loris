1776cecfc92bc1a22cb1f06006c5db1e7d4d92d0
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-06 22:23:36 -0300

    Add database creation to script for register sb

--- ci/scripts/register-service-broker.sh
+++ ci/scripts/register-service-broker.sh
@@ -10,6 +10,7 @@ source $project_dir/common/utils/cf-helpers.sh
 cf_authenticate_and_target
 cf_target_org_and_space system chaos-loris
 
+cf_create_service chaos-loris-broker p-mysql chaos-loris-broker-db
 
 cat > manifest.yml <<EOS
 ---
