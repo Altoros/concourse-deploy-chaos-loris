d23dcb7b12adfa5a925d86fb5e4d8654feb28031
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-06 22:11:11 -0300

    changed script

--- ci/scripts/deploy-service-broker.sh
+++ ci/scripts/deploy-service-broker.sh
@@ -4,8 +4,8 @@ set -e -x
 ### Load env
 
 mkdir -p ../go/src/github.com/Altoros/
+cp -r service-broker-repo/ ../go/src/github.com/Altoros/cf-chaos-loris-broker
 cd ..
-cp -r service-broker-repo/ go/src/github.com/Altoros/cf-chaos-loris-broker
 export GOPATH=$PWD/go/ 
 cd go/src/github.com/Altoros/cf-chaos-loris-broker
 #go get -d -t -v github.com/Altoros/cf-chaos-loris-broker
