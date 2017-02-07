9da6c321ebca4456d4c80679d39f881c35696504
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-06 22:45:16 -0300

    modified scripts

---
+++ ci/scripts/deploy-service-broker.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+### Load env
+
+project_dir=$(readlink -f "$(dirname $0)/../..")
+source $project_dir/common/utils/load-cf-env.sh
+source $project_dir/common/utils/cf-helpers.sh
+
+cf_authenticate_and_target
+cf_target_org_and_space system chaos-loris
+
+cf_create_service p-mysql 100mb-dev chaos-loris-broker-db
+
+cat > manifest.yml <<EOS
+---
+applications:
+- name: chaos-loris-broker
+  memory: 512M
+  instances: 2
+  buildpack: https://github.com/ryandotsmith/null-buildpack
+  path: .
+  command: ./cf-chaos-loris-broker -c plans.yml
+  env:
+    CHAOS_LORIS_HOST: https://chaos-loris.$CF_APPS_DOMAIN
+  services:
+  - chaos-loris-broker-db
+EOS
+cf push
+exit_on_error "Error pushing app"
+
--- ci/scripts/register-service-broker.sh
+++ ci/scripts/register-service-broker.sh
@@ -8,24 +8,16 @@ source $project_dir/common/utils/load-cf-env.sh
 source $project_dir/common/utils/cf-helpers.sh
 
 cf_authenticate_and_target
+exit_on_error "Error in auth"
+
 cf_target_org_and_space system chaos-loris
+exit_on_error "Error in cf taget command"
 
 cf_create_service p-mysql 100mb-dev chaos-loris-broker-db
+exit_on_error "Error creating service"
 
-cat > manifest.yml <<EOS
----
-applications:
-- name: chaos-loris-broker
-  memory: 512M
-  instances: 2
-  buildpack: https://github.com/ryandotsmith/null-buildpack
-  path: .
-  command: ./cf-chaos-loris-broker -c plans.yml
-  env:
-    CHAOS_LORIS_HOST: https://chaos-loris.$CF_APPS_DOMAIN
-  services:
-  - chaos-loris-broker-db
-EOS
-cf push
-exit_on_error "Error pushing app"
+register_broker 
+exit_on_error "Error registering broker"
 
+enable_global_access
+exit_on_error "Error enabling global access"
