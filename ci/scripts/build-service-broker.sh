3cb1f1a6555e7ff50505053f0f6f37a52d1eb148
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-07 14:25:24 -0300

    changed path

--- ci/scripts/build-service-broker.sh
+++ ci/scripts/build-service-broker.sh
@@ -3,15 +3,14 @@ set -e -x
 
 ### Load env
 
-mkdir -p ../go/src/github.com/Altoros/
-cp -r service-broker-repo/ ../go/src/github.com/Altoros/cf-chaos-loris-broker
-cd ..
+mkdir -p go/src/github.com/Altoros/
+cp -r service-broker-repo/ go/src/github.com/Altoros/cf-chaos-loris-broker
 export GOPATH=$PWD/go/ 
 export BINARY=$PWD/binary
 cd go/src/github.com/Altoros/cf-chaos-loris-broker
 
 go build 
 
-cp cf-chaos-loris-broker ../../../../../../../binary/
-cp plans.yml ../../../../../../../binary/
+cp cf-chaos-loris-broker ../../../../../../binary/
+cp plans.yml ../../../../../../binary/
 
