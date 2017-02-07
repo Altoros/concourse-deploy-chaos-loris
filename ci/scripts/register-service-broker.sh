e924f27a8231e2cdf5160f0c216a8f754084db09
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-07 14:41:36 -0300

    changed cf command

--- ci/scripts/register-service-broker.sh
+++ ci/scripts/register-service-broker.sh
@@ -16,7 +16,7 @@ exit_on_error "Error in cf taget command"
 cf_create_service p-mysql 100mb-dev chaos-loris-broker-db
 exit_on_error "Error creating service"
 
-register_broker p-mysql admin $CF_ADMIN_PASSWORD "https://chaos-loris-broker.$CF_APPS_DOMAIN"
+register_broker chaos-loris-broker admin $CF_ADMIN_PASSWORD "https://chaos-loris-broker.$CF_APPS_DOMAIN"
 exit_on_error "Error registering broker"
 
 enable_global_access "100mb-dev"
