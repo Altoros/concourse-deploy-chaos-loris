e96a31ec86b4eea66f94650f4fb99c2ef1f11b36
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-06 23:27:54 -0300

    .

--- ci/scripts/deploy-service-broker.sh
+++ ci/scripts/deploy-service-broker.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-set -e -x
 
 ### Load env
 
 project_dir=$(readlink -f "$(dirname $0)/../..")
 source $project_dir/common/utils/load-cf-env.sh
 source $project_dir/common/utils/cf-helpers.sh
+
+set -e -x
 
 cf_authenticate_and_target
 cf_target_org_and_space system chaos-loris
