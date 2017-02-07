048206fe63c5f22898faaa39e4fb01aee4f76af8
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-06 22:03:43 -0300

    changed pipeline build script

--- ci/scripts/deploy-service-broker.sh
+++ ci/scripts/deploy-service-broker.sh
@@ -3,6 +3,8 @@ set -e -x
 
 ### Load env
 
-export GOPATH=$PWD/service-broker-repo
-cd service-broker-repo
+cp -r service-broker-repo/ go/src/github.com/Altoros/cf-chaos-loris-broker
+export GOPATH=$PWD/go/ 
+cd go/src/github.com/Altoros/cf-chaos-loris-broker
+go get -d -t -v github.com/Altoros/cf-chaos-loris-broker
 go build 
