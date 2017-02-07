5a37d7203462f3d1ee0fe345ece425d5abbc83db
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-07 14:28:44 -0300

    changed path in deploy script

--- ci/scripts/deploy-service-broker.sh
+++ ci/scripts/deploy-service-broker.sh
@@ -8,7 +8,6 @@ project_dir=$(readlink -f "$(dirname $0)/../..")
 source $project_dir/common/utils/load-cf-env.sh
 source $project_dir/common/utils/cf-helpers.sh
 
-
 cf_authenticate_and_target
 cf_target_org_and_space system chaos-loris
 
@@ -26,7 +25,7 @@ applications:
   services:
   - chaos-loris-broker-db
 EOS
-
+cd binary
 echo "##############################"
 cf push
 exit_on_error "Error pushing app"
