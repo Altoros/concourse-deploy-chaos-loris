f193ce74df1f76344a338b497b751e859dedb409
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-07 14:10:16 -0300

    changed path

--- ci/scripts/build-service-broker.sh
+++ ci/scripts/build-service-broker.sh
@@ -7,8 +7,10 @@ mkdir -p ../go/src/github.com/Altoros/
 cp -r service-broker-repo/ ../go/src/github.com/Altoros/cf-chaos-loris-broker
 cd ..
 export GOPATH=$PWD/go/ 
+export BINARY=$PWD/binary
 cd go/src/github.com/Altoros/cf-chaos-loris-broker
 #go get -d -t -v github.com/Altoros/cf-chaos-loris-broker
 go build 
-cp cf-chaos-loris-broker ../binary/
-cp plans.yml ../binary/
+cp cf-chaos-loris-broker $BINARY/
+cp plans.yml $BINARY/
+
