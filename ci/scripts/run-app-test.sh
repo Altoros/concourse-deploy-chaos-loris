e1e9c6fc3c6b2ff0407f8c3ad9374514d2e4332d
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-08 14:35:22 -0300

    modified run-app-test

--- ci/scripts/run-app-test.sh
+++ ci/scripts/run-app-test.sh
@@ -22,13 +22,29 @@ CF_CL_URL=chaos-loris.$CF_APPS_DOMAIN
 VICTIM_APP_NAME=simple-victim-app
 APP_GUID=$(cf curl "/v2/apps" -X GET -H "Content-Type: application/x-www-form-urlencoded" -d "q=name:$VICTIM_APP_NAME" | jq -r .resources[0].metadata.guid)
 
-if curl -k "https://$CF_CL_URL/applications" -i -X GET -H 'Content-Type: application/json' | grep $APP_GUID;
-then
+APP_URL=`curl -k "https://$CF_CL_URL/applications" -i -X GET -H 'Content-Type: application/json' | tail -1 | jq -r ._embedded.applications[] | select ( .applicationId == "$APP_GUID") ._links.self.href`;
+if echo $APP_URL | grep $APP_GUID;
+then                              
    curl -k "https://$CF_CL_URL/applications/$APP_GUID" -i -X DELETE -H 'Content-Type: application/json'
-fi
-
-# This curl will return the url of all apps
-APP_URL=`curl -k "https://$CF_CL_URL/applications" -i -X POST -H 'Content-Type: application/json' -d "{ 
-  \"applicationId\" : \"$APP_GUID\"
-}" | grep applicationID | awk -F: '{print $2}'`
+fi   
 echo $APP_URL;
+
+SCHED_HASH=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+
+echo "########################################"
+echo " SCHEDULE HASH IS: $SCHED_HASH"
+echo "########################################"
+
+SCHEDULE_URL= `curl -k "https://$CF_CL_URL/schedules" -i -X POST -H 'Content-Type: application/json' -d "{
+  \"name\" : \"$SCHED_HASH\",
+  \"expression\" : \"*/3 * * * * *\"
+}" | tail -1 | jq '.'`;
+
+exit 0
+curl -k "https://$CF_CL_URL -i -X POST -H 'Content-Type: application/json' -d "{
+  \"schedule\" : \"$SCHEDULE_URL\",
+  \"application\" : \"$APP_URL",
+  \"probability\" : 0.3
+}"
+
+
