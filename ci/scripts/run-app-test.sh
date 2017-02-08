541a54c6f3c6a8a604e8e9412f14e7826522b69f
Andres Garcia Fiorini <andres.fiorini@gmail.com>
2017-02-07 21:35:54 -0300

    .

--- ci/scripts/run-app-test.sh
+++ ci/scripts/run-app-test.sh
@@ -13,6 +13,7 @@ VICTIM_APP_NAME=$1
 APP_GUID=$(cf curl "/v2/apps" -X GET -H "Content-Type: application/x-www-form-urlencoded" -d "q=name:$VICTIM_APP_NAME" | jq -r .resources[0].metadata.guid)
 
 # This curl will return the url of all apps
+set -x
 LIST_APPS_URL=`curl -k "https://$CF_CL_URL/applications" -i -X POST -H 'Content-Type: application/json' -d "{
   \"applicationId\" : \"$APP_GUID\"
 }"`
